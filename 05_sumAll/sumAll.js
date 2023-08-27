7cfe94070adc0b5c639907f257444292bd95c99e
Kekgethilwe Moloi <61177186+KekgethilweMoloi@users.noreply.github.com>
2023-08-28 00:20:53 +0200

    Remove .skip from spec and add code to sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if(min < 0 || max < 0){
+        return "ERROR";
+    }
+    if(!Number.isInteger(min) || !Number.isInteger(max))
+    {
+        return "ERROR";
+    }
+    if(min < max){
+        return((max-min) + 1) * (min + max) / 2;
+    }
+    if(min > max){
+         return ((min - max) + 1) * (min + max) / 2;
+    }
 };
 
 // Do not edit below this line
