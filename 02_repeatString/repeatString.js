5c7f17633ddebb5b0bd8fd2e582a0e45a1b9fd20
Kekgethilwe Moloi <61177186+KekgethilweMoloi@users.noreply.github.com>
2023-08-27 20:28:25 +0200

    Edit repeatString.js and repeatrString.spec.js
    Added the code in repeatString.js to ensure tests would pass
    Removed the .skip to the test to ensure the test would run

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeats){
+    if(repeats < 0 ){
+        return "ERROR";
+    }
+    let newString = "";
+    for(let i = 0; i < repeats; i++){
+        newString += word;
+    }
+    return newString;
 };
 
 // Do not edit below this line
