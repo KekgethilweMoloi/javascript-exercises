cb87dbb6521053775cfdb5fedd6ee15ea999f98b
Kekgethilwe Moloi <61177186+KekgethilweMoloi@users.noreply.github.com>
2023-08-27 23:06:06 +0200

    Remove skip from spec, add code to removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...param) {
+    const newArray = [];
+    arr.forEach(item => {
+        if(!param.includes(item)){
+            newArray.push(item);
+        }
+    });
 
+    return newArray;
 };
 
 // Do not edit below this line
