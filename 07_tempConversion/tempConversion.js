86600e16b88f75c7675d0252f29d3c528c02e78b
Kekgethilwe Moloi <61177186+KekgethilweMoloi@users.noreply.github.com>
2023-08-30 20:37:14 +0200

    Remove skip from spec and add temp conversion code
    Removed skip to allow tests to run
    Added functions for conversion and if statements to deal with rounding
    or lack thereof

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
-};
+let tempCalc;
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(temp) {
+  tempCalc = (temp - 32) * 5 / 9;
+  if(tempCalc % 1 !== 0){
+    return Math.round(tempCalc * 10)/ 10;
+  } 
+  else{
+    return tempCalc ;
+  }
+};
+  
+const convertToFahrenheit = function(temp) {
+  tempCalc = (temp * 9/5 + 32);
+  if(tempCalc % 1 !== 0){ 
+     return Math.round(tempCalc * 10)/ 10;
+  }
+  else{
+    return tempCalc;
+  }
 };
 
 // Do not edit below this line
